003b0084c610f1f87e991b6b8d15626987c131c7
pjmbt <58105184+pjmbt@users.noreply.github.com>
2020-05-05 11:55:04 +0700

    Add files via upload

---
+++ predict/NetBeansProjects/ClientApp/build/generated/jax-wsCache/ServerWebService/serverpack/Operation.java
@@ -0,0 +1,141 @@
+
+package serverpack;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for operation complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="operation">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="k" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="b" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="sex" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="old" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "operation", propOrder = {
+    "k",
+    "b",
+    "sex",
+    "old"
+})
+public class Operation {
+
+    protected String k;
+    protected String b;
+    protected String sex;
+    protected String old;
+
+    /**
+     * Gets the value of the k property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getK() {
+        return k;
+    }
+
+    /**
+     * Sets the value of the k property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setK(String value) {
+        this.k = value;
+    }
+
+    /**
+     * Gets the value of the b property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getB() {
+        return b;
+    }
+
+    /**
+     * Sets the value of the b property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setB(String value) {
+        this.b = value;
+    }
+
+    /**
+     * Gets the value of the sex property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * Sets the value of the sex property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSex(String value) {
+        this.sex = value;
+    }
+
+    /**
+     * Gets the value of the old property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getOld() {
+        return old;
+    }
+
+    /**
+     * Sets the value of the old property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setOld(String value) {
+        this.old = value;
+    }
+
+}
